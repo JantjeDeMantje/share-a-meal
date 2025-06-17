ff9462e2d62b23e22702e98b48647b98ba74ac6d
JanRoelofs <janroe2004@gmail.com>
2025-06-17 15:00:33 +0200

    Fix for deployed api

--- app.js
+++ app.js
@@ -18,6 +18,6 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     res.send('API is running!');
 });
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+console.log(`Server running on port ${PORT}`);
 
 export default app;
